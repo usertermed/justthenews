966298125b380a4f4f7fb64cb17267a8bb1b021c
None <damiensully413@gmail.com>
2025-12-20 15:30:24 -0600

    Change loading news

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <main className="flex min-h-screen w-full items-center justify-center bg-background p-12">
       <div className="font-headline text-5xl md:text-7xl lg:text-8xl font-thin text-muted-foreground text-center leading-tight animate-pulse">
-        Loading News...
+        ...
       </div>
     </main>
   );
